4098f27a9f86485e2ac9aa2165e071a084310f8d
premthecoder <premsingh.me@gmail.com>
2019-02-13 17:23:12 +0530

    Updates

---
+++ precache-manifest.c31735786a5e159ca4b91369fff745a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/post-board/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e265156a61fc28fe41e",
+    "url": "/post-board/static/js/runtime~main.3e265156.js"
+  },
+  {
+    "revision": "0f06c32586058efbbac7",
+    "url": "/post-board/static/js/main.0f06c325.chunk.js"
+  },
+  {
+    "revision": "fae525bdbd156a64300f",
+    "url": "/post-board/static/js/2.fae525bd.chunk.js"
+  },
+  {
+    "revision": "0f06c32586058efbbac7",
+    "url": "/post-board/static/css/main.e066123e.chunk.css"
+  },
+  {
+    "revision": "fae525bdbd156a64300f",
+    "url": "/post-board/static/css/2.ae654a44.chunk.css"
+  },
+  {
+    "revision": "84238617d024d2169b494329b361e2a5",
+    "url": "/post-board/index.html"
+  }
+];
